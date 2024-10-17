465b0f2e934d75cd054a4f0c59b9d731232880ef
divyasreedivya <tekkupallidivyasree@gmail.com>
2024-10-17 12:09:37 +0530

    Create n+1int.py

---
+++ n+1int.py
@@ -0,0 +1,29 @@
+'''
+2)Write a program to create a list and display it. 
+Input format:
+Input consist of n+1 integers
+First integer corresponds to the size of the list
+Next n inputs corresponds to the elements in the list 
+Output format: 
+Output is an integer list
+Sample Input
+4 
+1
+2
+3
+4
+Output
+1 2 3 4
+'''
+n = int(input())
+
+# Initialize an empty list
+my_list = []
+
+# Input the list elements
+for _ in range(n):
+    element = int(input())
+    my_list.append(element)
+
+# Output the list elements separated by spaces
+print(" ".join(map(str, my_list)))
