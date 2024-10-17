3523b38ab63d418cb8ce8c946338c77226b9738c
divyasreedivya <tekkupallidivyasree@gmail.com>
2024-10-17 12:10:35 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,33 @@
+'''
+3)Write a Python Program to find the largest number in a list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the size of the list.
+Output consists of the largest element.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+6
+'''
+# Input the size of the list
+size = int(input())
+
+# Initialize an empty list
+my_list = []
+
+# Input the list elements
+for _ in range(size):
+    element = int(input())
+    my_list.append(element)
+
+# Find the largest element
+largest = max(my_list)
+
+# Output the largest element
+print(largest)
